8e42b1cbdccece82342bf3ad30ef22c9613e5b9d
Will Norris <will@willnorris.com>
2013-12-30 13:40:37 -0800

    update to outreach 3.0

--- functions.php
+++ functions.php
@@ -2,49 +2,58 @@
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
-//* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Outreach Theme' );
-define( 'CHILD_THEME_URL', 'http://my.studiopress.com/themes/outreach' );
-define( 'CHILD_THEME_VERSION', '2.0.1' );
-
 //* Set Localization (do not remove)
 load_child_theme_textdomain( 'outreach', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'outreach' ) );
 
-//* Sets Content Width
-$content_width = apply_filters( 'content_width', 650, 450, 960 );
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', __( 'Outreach Pro Theme', 'outreach' ) );
+define( 'CHILD_THEME_URL', 'http://my.studiopress.com/themes/outreach/' );
+define( 'CHILD_THEME_VERSION', '3.0.0' );
 
-//* Load Google fonts
-add_action( 'wp_enqueue_scripts', 'outreach_load_google_fonts' );
-function outreach_load_google_fonts() {
-    wp_enqueue_style( 
-    	'google-fonts', 
-    	'//fonts.googleapis.com/css?family=Lato:400,700', 
-    	array(), 
-    	CHILD_THEME_VERSION 
-    );
-}
+//* Add HTML5 markup structure
+add_theme_support( 'html5' );
 
-//* Add Viewport meta tag for mobile browsers
+//* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add new image sizes
-add_image_size( 'featured', 1040, 400, TRUE );
-add_image_size( 'home-sections', 215, 140, TRUE );
-add_image_size( 'sidebar', 300, 150, TRUE );
+//* Enqueue Google fonts
+add_action( 'wp_enqueue_scripts', 'outreach_google_fonts' );
+function outreach_google_fonts() {
 
-//* Create additional color style options
-add_theme_support( 'genesis-style-selector', array(
-	'outreach-blue' 	=>	__( 'Blue', 'outreach' ),
-	'outreach-orange' 	=> 	__( 'Orange', 'outreach' ),
-	'outreach-red' 		=> 	__( 'Red', 'outreach' ) 
-) );
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:400,700', array(), CHILD_THEME_VERSION );
+	
+}
+
+//* Enqueue Responsive Menu Script
+add_action( 'wp_enqueue_scripts', 'outreach_enqueue_responsive_script' );
+function outreach_enqueue_responsive_script() {
+
+	wp_enqueue_script( 'outreach-responsive-menu', get_bloginfo( 'stylesheet_directory' ) . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0' );
+
+}
+
+//* Add new image sizes
+add_image_size( 'home-top', 1140, 460, TRUE );
+add_image_size( 'home-bottom', 285, 160, TRUE );
+add_image_size( 'sidebar', 300, 150, TRUE );
 
 //* Add support for custom header
 add_theme_support( 'custom-header', array(
-    'width'         	=> 330,
-    'height'        	=> 100,
-    'header-selector'   => '#title-area a',
-    'header-text'       => false
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'height'          => 100,
+	'width'           => 340,
+) );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for additional color style options
+add_theme_support( 'genesis-style-selector', array(
+	'outreach-pro-blue' 	=>	__( 'Outreach Pro Blue', 'outreach' ),
+	'outreach-pro-orange' 	=> 	__( 'Outreach Pro Orange', 'outreach' ),
+	'outreach-pro-purple' 	=> 	__( 'Outreach Pro Purple', 'outreach' ),
+	'outreach-pro-red' 		=> 	__( 'Outreach Pro Red', 'outreach' ),
 ) );
 
 //* Add support for structural wraps
@@ -52,81 +61,110 @@ add_theme_support( 'genesis-structural-wraps', array(
 	'header',
 	'nav',
 	'subnav',
-	'inner',
+	'site-inner',
 	'footer-widgets',
-	'footer'
+	'footer',
 ) );
 
-//* Reposition the secondary navigation
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_subnav' );
+//* Add support for 4-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 4 );
+
+//* Set Genesis Responsive Slider defaults
+add_filter( 'genesis_responsive_slider_settings_defaults', 'outreach_responsive_slider_defaults' );
+function outreach_responsive_slider_defaults( $defaults ) {
+
+	$args = array(
+		'location_horizontal'             => 'Left',
+		'location_vertical'               => 'bottom',
+		'posts_num'                       => '4',
+		'slideshow_excerpt_content_limit' => '100',
+		'slideshow_excerpt_content'       => 'full',
+		'slideshow_excerpt_width'         => '35',
+		'slideshow_height'                => '460',
+		'slideshow_more_text'             => __( 'Continue Reading', 'outreach' ),
+		'slideshow_title_show'            => 1,
+		'slideshow_width'                 => '1140',
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+	
+	return $args;
+}
+
+//* Hook after post widget after the entry content
+add_action( 'genesis_after_entry', 'outreach_after_entry', 5 );
+function outreach_after_entry() {
+
+	if ( is_singular( 'post' ) )
+		genesis_widget_area( 'after-entry', array(
+			'before' => '<div class="after-entry widget-area">',
+			'after'  => '</div>',
+		) );
+
+}
 
 //* Modify the size of the Gravatar in the author box
 add_filter( 'genesis_author_box_gravatar_size', 'outreach_author_box_gravatar_size' );
 function outreach_author_box_gravatar_size( $size ) {
+
     return '80';
+    
+}
+
+//* Remove comment form allowed tags
+add_filter( 'comment_form_defaults', 'mpp_remove_comment_form_allowed_tags' );
+function mpp_remove_comment_form_allowed_tags( $defaults ) {
+	
+	$defaults['comment_notes_after'] = '';
+	return $defaults;
+
 }
 
 //* Add the sub footer section
 add_action( 'genesis_before_footer', 'outreach_sub_footer', 5 );
 function outreach_sub_footer() {
+
 	if ( is_active_sidebar( 'sub-footer-left' ) || is_active_sidebar( 'sub-footer-right' ) ) {
-		echo '<div id="sub-footer"><div class="wrap">';
+		echo '<div class="sub-footer"><div class="wrap">';
 		
 		   genesis_widget_area( 'sub-footer-left', array(
-		       'before' => '<div class="sub-footer-left">'
+		       'before' => '<div class="sub-footer-left">',
+		       'after'  => '</div>',
 		   ) );
 	
 		   genesis_widget_area( 'sub-footer-right', array(
-		       'before' => '<div class="sub-footer-right">'
+		       'before' => '<div class="sub-footer-right">',
+		       'after'  => '</div>',
 		   ) );
 	
-		echo '</div><!-- end .wrap --></div><!-- end #sub-footer -->';	
+		echo '</div><!-- end .wrap --></div><!-- end .sub-footer -->';	
 	}
-}
-
-//* Add support for 4-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 4 );
-
-//* Customizes go to top text
-add_filter( 'genesis_footer_backtotop_text', 'footer_backtotop_filter' );
-function footer_backtotop_filter($backtotop) {
-	return '[footer_backtotop text="' . __('Top' , 'outreach' ) . '"]';
+	
 }
 
 //* Register widget areas
 genesis_register_sidebar( array(
-	'id'				=> 'home-featured',
-	'name'			=> __( 'Home Featured', 'outreach' ),
-	'description'	=> __( 'This is the home featured section.', 'outreach' )
+	'id'          => 'home-top',
+	'name'        => __( 'Home - Top', 'outreach' ),
+	'description' => __( 'This is the top section of the Home page.', 'outreach' ),
 ) );
 genesis_register_sidebar( array(
-	'id'				=> 'home-1',
-	'name'			=> __( 'Home #1', 'outreach' ),
-	'description'	=> __( 'This is the home #1 section.', 'outreach' )
+	'id'          => 'home-bottom',
+	'name'        => __( 'Home - Bottom', 'outreach' ),
+	'description' => __( 'This is the bottom section of the Home page.', 'outreach' ),
 ) );
 genesis_register_sidebar( array(
-	'id'				=> 'home-2',
-	'name'			=> __( 'Home #2', 'outreach' ),
-	'description'	=> __( 'This is the home #2 section.', 'outreach' )
+	'id'          => 'after-entry',
+	'name'        => __( 'After Entry', 'outreach' ),
+	'description' => __( 'This is the after entry widget area.', 'outreach' ),
 ) );
 genesis_register_sidebar( array(
-	'id'				=> 'home-3',
-	'name'			=> __( 'Home #3', 'outreach' ),
-	'description'	=> __( 'This is the home #3 section.', 'outreach' )
+	'id'          => 'sub-footer-left',
+	'name'        => __( 'Sub Footer - Left', 'outreach' ),
+	'description' => __( 'This is the left section of the sub footer.', 'outreach' ),
 ) );
 genesis_register_sidebar( array(
-	'id'				=> 'home-4',
-	'name'			=> __( 'Home #4', 'outreach' ),
-	'description'	=> __( 'This is the home #4 section.', 'outreach' )
-) );
-genesis_register_sidebar( array(
-	'id'				=> 'sub-footer-left',
-	'name'			=> __( 'Sub Footer Left', 'outreach' ),
-	'description'	=> __( 'This is the sub footer left section.', 'outreach' )
-) );
-genesis_register_sidebar( array(
-	'id'				=> 'sub-footer-right',
-	'name'			=> __( 'Sub Footer Right', 'outreach' ),
-	'description'	=> __( 'This is the sub footer right section.', 'outreach' )
+	'id'          => 'sub-footer-right',
+	'name'        => __( 'Sub Footer - Right', 'outreach' ),
+	'description' => __( 'This is the right section of the sub footer.', 'outreach' ),
 ) );
--- page_landing.php
+++ page_landing.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * This file adds the Landing template to the Outreach Child Theme.
+ * This file adds the Landing template to the Outreach Pro Theme.
  *
  * @author StudioPress
- * @package Generate
+ * @package Outreach Pro
  * @subpackage Customizations
  */
 
@@ -11,25 +11,40 @@
 Template Name: Landing
 */
 
-// Add custom body class to the head
+//* Add custom body class to the head
 add_filter( 'body_class', 'outreach_add_body_class' );
 function outreach_add_body_class( $classes ) {
-   $classes[] = 'outreach-landing';
+
+   $classes[] = 'outreach-pro-landing';
    return $classes;
+   
 }
 
-// Remove header, navigation, breadcrumbs, footer widgets, footer 
-add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+//* Force full width content layout
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+//* Remove site header elements
 remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
-remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_header', 'genesis_do_header', 10 );
 remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+
+//* Remove navigation
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
-remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_subnav' );
-remove_action( 'genesis_after_header', 'genesis_do_breadcrumbs');
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+
+//* Remove breadcrumbs
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+//* Remove sub-footer widgets
 remove_action( 'genesis_before_footer', 'outreach_sub_footer', 5 );
+
+//* Remove site footer widgets
 remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+
+//* Remove site footer elements
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
 
+//* Run the Genesis loop
 genesis();
