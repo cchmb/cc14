3d0781858fba9c10e420d51737154c81d0ced832
Will Norris <will@willnorris.com>
2020-11-09 14:32:33 -0800

    display ctc sermon data

--- single-ctc_sermon.php
+++ single-ctc_sermon.php
@@ -11,4 +11,37 @@ if ( is_active_sidebar( 'sermon-sidebar' ) ) {
   });
 }
 
+add_filter('genesis_entry_content', function() {
+    extract( ctfw_sermon_data() );
+
+    if ($video) {
+?>
+  <section class="sermon_video">
+    <div class="wrap">
+      <?php echo $video_player; ?>
+    </div>
+    <p class="download_link"><a href="<?php esc_attr_e($video) ?>">Watch on YouTube</a></p>
+  </section>
+      <?php
+    }
+
+    if ($audio) { ?>
+      <section class="sermon_audio"><h3>Audio</h3>
+        <?php echo $audio_player; ?>
+        <p class="download_link"><a href="<?php esc_attr_e($audio) ?>">Download audio file</a></p>
+      </section>
+    <?php }
+
+    if ($slides) {
+      preg_match("#https://docs.google.com/presentation/d/([^/]+).*#", $slides, $matches);
+      if ($id = $matches[1]) { ?>
+        <section class="sermon_slides"><h3>Sermon Slides</h3>
+          <div class="wrap">
+            <iframe src="https://docs.google.com/presentation/d/<?php esc_attr_e($id) ?>/embed" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </section>
+      <?php }
+    }
+}, 9);
+
 genesis();
